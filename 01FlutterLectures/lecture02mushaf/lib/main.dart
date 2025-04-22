876f5a8ebb5a584b99d5142fe7b8503283d40ba3
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-04-22 08:39:52 +0500

    Lecture series of dart

---
+++ 01FlutterLectures/lecture02mushaf/lib/main.dart
@@ -0,0 +1,153 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:quran/quran.dart' as quran;
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashSCR(),
+    );
+  }
+}
+
+class SplashSCR extends StatefulWidget {
+  const SplashSCR({super.key});
+
+  @override
+  State<SplashSCR> createState() => _SplashSCRState();
+}
+
+class _SplashSCRState extends State<SplashSCR> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SurahIndexSCR()));
+    });
+
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[900],
+      body: Center(child: Image.asset("assets/images/quran.png")),
+    );
+  }
+}
+
+class SurahIndexSCR extends StatefulWidget {
+  const SurahIndexSCR({super.key});
+
+  @override
+  State<SurahIndexSCR> createState() => _SurahIndexSCRState();
+}
+
+class _SurahIndexSCRState extends State<SurahIndexSCR> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: quran.totalSurahCount,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailSurah(index + 1),
+                  ));
+            },
+            leading: CircleAvatar(
+              child: Text("${index + 1}"),
+            ),
+            title: Text(
+              quran.getSurahNameArabic(index + 1),
+              style: GoogleFonts.amiriQuran(),
+            ),
+            subtitle: Text(quran.getSurahNameEnglish(index + 1)),
+            trailing: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                quran.getPlaceOfRevelation(index + 1) == "Makkah"
+                    ? Image.asset(
+                        "assets/images/kaaba.png",
+                        width: 30,
+                        height: 30,
+                      )
+                    : Image.asset(
+                        "assets/images/madina.png",
+                        width: 30,
+                        height: 30,
+                      ),
+                Text("verses" + quran.getVerseCount(index+1).toString())
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailSurah extends StatefulWidget {
+  var surahNum;
+  DetailSurah(this.surahNum, {super.key});
+
+  @override
+  State<DetailSurah> createState() => _DetailSurahState();
+}
+
+class _DetailSurahState extends State<DetailSurah> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(quran.getSurahName(widget.surahNum)),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: ListView.builder(
+            itemCount: quran.getVerseCount(widget.surahNum),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  quran.getVerse(widget.surahNum, index + 1,
+                      verseEndSymbol: true),
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.amiriQuran(),
+                ),
+                subtitle: Text(
+                  quran.getVerseTranslation(widget.surahNum, index + 1,
+                     
+                      translation: quran.Translation.urdu),
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontFamily: "jameel"),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
